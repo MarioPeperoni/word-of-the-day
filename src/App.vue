beb5bb84e3f0fb4d9ec327e580ae7e6fe19d1389
Mateusz Wiącek <68820849+MarioPeperoni@users.noreply.github.com>
2025-04-12 00:14:14 +0200

    initial: initial project commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import WordInput from './components/WordInput.vue'
+
+const today = new Date().toLocaleDateString('pl-PL', {
+  month: 'long',
+  day: '2-digit',
+})
+
+const weekDay = new Date().toLocaleDateString('pl-PL', {
+  weekday: 'long',
+})
+
+const showHistoric = () => {
+  console.log('Historyczne słowa dnia')
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      Słowo dnia - <span class="weekday">{{ weekDay }}</span
+      >, {{ today }}
+    </h1>
+  </header>
+  <main>
+    <WordInput />
+  </main>
+  <footer>
+    <a @click="showHistoric">Historyczne słowa dnia</a>
+  </footer>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+footer {
+  text-align: center;
+  margin-top: 20px;
+}
+
+a {
+  color: #ff6f2f;
+  text-decoration: underline;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.weekday {
+  color: #ff6f2f;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/WordInput.vue
@@ -0,0 +1,66 @@
+<script lang="ts">
+import { ref } from 'vue'
+
+const word = ref('')
+</script>
+
+<template>
+  <div class="word-input-container">
+    <input
+      v-model="word"
+      type="text"
+      @keyup.enter="submitWord"
+      class="word-input"
+      placeholder="Słowo dnia"
+    />
+    <!-- <button @click="submitWord" class="submit-button" :style="{ opacity: word ? '100%' : '0%' }">
+      Submit
+    </button> -->
+  </div>
+</template>
+
+<style scoped>
+.word-input-container {
+  max-width: 100%;
+  margin: 0 auto;
+  text-align: center;
+  margin-top: auto;
+  margin-bottom: auto;
+  padding: 30px;
+}
+
+.word-input {
+  width: 100%;
+  padding: 20px 10px;
+  font-size: 80px;
+  border: none;
+  border-bottom: 2px solid #ffffff;
+  background-color: transparent;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  transition: border-color 0.3s;
+}
+
+.word-input:focus {
+  border-color: #ff6f2f;
+  outline: none;
+}
+
+.submit-button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 20px;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  background-color: #ff6f2f;
+  font-weight: 600;
+}
+
+.submit-button:hover {
+  background-color: #fc824e;
+}
+</style>
